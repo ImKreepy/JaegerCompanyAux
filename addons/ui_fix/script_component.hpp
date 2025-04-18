86b93f038171974fd846b1b1af1af71201a8c8f9
Im Kreepy <jkoscinski3@hotmail.com>
2025-04-18 15:24:03 -0400

    Fixed Component name

--- addons/ui_fix/script_component.hpp
+++ addons/ui_fix/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT remove_menu
-#define COMPONENT_BEAUTIFIED Remove Menu
+#define COMPONENT ui_fix
+#define COMPONENT_BEAUTIFIED UI Fix
 #include "\z\JC\addons\main\script_mod.hpp"
 
 #include "\z\JC\addons\main\script_macros.hpp"
