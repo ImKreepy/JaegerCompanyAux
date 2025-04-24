2c9d7b8517d50e132fad383d729fbd650988dd2a
Im Kreepy <jkoscinski3@hotmail.com>
2025-04-24 02:10:06 -0400

    Couldnt figure it out

--- addons/acre2_aux/CfgAcreRadios.hpp
+++
@@ -1,9 +0,0 @@
-class CfgAcreComponents
-{
-    class ACRE_PRC117F_base;
-    class ACRE_PRC117F: ACRE_PRC117F_base {};
-    class IK_ACRE_PRC118: ACRE_PRC117F
-    {
-        name = "AN/PRC-118";
-    };
-};
