1c2865e29d8cbf77eec49f66116519a5e420ede6
Im Kreepy <53015099+ImKreepy@users.noreply.github.com>
2025-04-23 21:24:55 -0400

    Create script_component.hpp
    ACRE2 Auxiliary script componenent

---
+++ addons/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT acre2_aux
+#define COMPONENT_BEAUTIFIED Advanced Combat Radio Environment Auxiliary
+
+#include "\z\jc\addons\main\script_mod.hpp"
+
+#include "\z\jc\addons\main\script_macros.hpp"
