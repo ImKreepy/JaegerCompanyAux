340d1ac844117276e03491ceef09dc42828afa71
Im Kreepy <jkoscinski3@hotmail.com>
2025-04-25 02:02:40 -0400

    I lied to myself

--- addons/tcp_aux/mapo_compat/config.cpp
+++ addons/tcp_aux/mapo_compat/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
         name = COMPONENT_NAME;
         addonRootClass = QUOTE(ADDON);
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QUOTE(ADDON),"A3_Ui_F_TCP","jc_misriah_aux_mapo"};
+        requiredAddons[] = {QUOTE(ADDON),"jc_misriah_aux_mapo"};
         skipWhenMissingDependencies = 1;
         units[] = { };
         weapons[] = {   };
