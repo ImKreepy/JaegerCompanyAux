ec49f5a238b1e7a1176761f2d14b565a8319803d
Im Kreepy <jkoscinski3@hotmail.com>
2025-06-09 00:51:05 -0400

    Added 1st MEU support

---
+++ addons/optre_aux/meu_aux/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+    class SUBADDON {
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {QUOTE(ADDON)};
+        skipWhenMissingDependencies = 1;
+        units[] = { };
+        weapons[] = {   };
+        VERSION_CONFIG;
+    };
+};
