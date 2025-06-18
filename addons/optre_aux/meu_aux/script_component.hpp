b351c11a91d228df405566e4bca38f7088578997
Im Kreepy <jkoscinski3@hotmail.com>
2025-06-18 15:04:36 -0400

    Fixed redefining macro

--- addons/optre_aux/meu_aux/script_component.hpp
+++ addons/optre_aux/meu_aux/script_component.hpp
@@ -1,3 +1,3 @@
-#define COMPONENT meu_aux
-#define COMPONENT_BEAUTIFIED 1st MEU Auxiliary
+#define SUBCOMPONENT meu_aux
+#define SUBCOMPONENT_BEAUTIFIED 1st MEU Auxiliary
 #include "..\script_component.hpp"
