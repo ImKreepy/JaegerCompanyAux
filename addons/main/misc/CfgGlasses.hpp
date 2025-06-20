8a874a8f31d175be18975e48a167e5aa3c64aeda
Im Kreepy <jkoscinski3@hotmail.com>
2025-06-20 01:56:39 -0400

    Added picture config

--- addons/main/misc/CfgGlasses.hpp
+++ addons/main/misc/CfgGlasses.hpp
@@ -7,6 +7,7 @@ class CfgGlasses
         scopeCurator = 2;
         scopeArsenal = 2;
         displayName = ITEM_NAME(Empty Glasses);
+        picture = ITEM_PICTURE(picture_ca);
         model = "\a3\weapons_f\empty.p3d";
         author = AUTHOR;
         ACE_Resistance = 2;
@@ -14,7 +15,8 @@ class CfgGlasses
     };
     class IK_Glasses_ODST : IK_Glasses_Empty
     {
-        displayName = ITEM_NAME(Empty Glasses);
+        displayName = ITEM_NAME(ODST Visor);
+        picture = ITEM_PICTURE(picture_ca);
         ACE_Overlay = QPATHTOF(textures\CombatGoggles.paa);
         ACE_OverlayCracked = QPATHTOF(textures\CombatGogglesCracked.paa);
     };
