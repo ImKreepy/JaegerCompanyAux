afbf34e1a64a2fbd65d81c343ad5ac1fcce8c980
Im Kreepy <jkoscinski3@hotmail.com>
2025-07-04 02:08:05 -0400

    Changed main prefix to "ik"

--- mod.cpp
+++ mod.cpp
@@ -8,12 +8,12 @@ actionName = "Join Jaeger Company";
 tooltip = "Jaeger Company Unit Auxiliary";
 tooltipOwned = "Jaeger Company Unit Auxiliary Owned";
 
-picture = "\z\jc\addons\main\logos\jc_logotitle_512_ca.paa"; // Logo displayed in expansions menu
-logo = "\z\jc\addons\main\logos\jc_logo_ca.paa"; // Image displayed on the main menu (128x)
-logoOver = "\z\jc\addons\main\logos\jc_logoover_ca.paa"; // Image displayed when the mouse hovers over the image on the main menu (128x)
-logoSmall = "\z\jc\addons\main\logos\jc_logo_small_ca.paa"; // Image displayed in Arsenal or 3DEN (32x)
+picture = "\ik\jc\addons\main\logos\jc_logotitle_512_ca.paa"; // Logo displayed in expansions menu
+logo = "\ik\jc\addons\main\logos\jc_logo_ca.paa"; // Image displayed on the main menu (128x)
+logoOver = "\ik\jc\addons\main\logos\jc_logoover_ca.paa"; // Image displayed when the mouse hovers over the image on the main menu (128x)
+logoSmall = "\ik\jc\addons\main\logos\jc_logo_small_ca.paa"; // Image displayed in Arsenal or 3DEN (32x)
 
 overview = "This mod provides auxiliary support for the Jaeger Company unit.";
 overviewText = "Jaeger Company Unit Auxiliary Mod";
-overviewPicture = "\z\jc\addons\main\logos\jc_logo_ca.paa";
-overviewFootnote = "\z\jc\addons\main\logos\jc_logo_ca.paa";
+overviewPicture = "\ik\jc\addons\main\logos\jc_logo_ca.paa";
+overviewFootnote = "\ik\jc\addons\main\logos\jc_logo_ca.paa";
