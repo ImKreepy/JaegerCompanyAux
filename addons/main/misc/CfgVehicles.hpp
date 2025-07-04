0f388cc57c9341cc7784a33819945d649f7db741
Im Kreepy <53015099+ImKreepy@users.noreply.github.com>
2025-07-04 15:12:06 -0400

    Update CfgVehicles.hpp

--- addons/main/misc/CfgVehicles.hpp
+++ addons/main/misc/CfgVehicles.hpp
@@ -26,34 +26,24 @@ class CfgVehicles
 		scope = 1;
   author = AUTHOR;
 		displayName = "Rescale Objects";
-		//icon = "\TAG_addonName\data\icon_Nuke_ca.paa";
 		category = "IK_Modules";
 
 		function = QFUNCMAIN(rescaleObjects);	// Name of function triggered once conditions are met
 		functionPriority = 1;				// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
-		isGlobal = 1;						// 0 for server only execution, 1 for global execution, 2 for persistent global execution
-		isTriggerActivated = 0;				// 1 for module waiting until all synced triggers are activated
-		isDisposable = 1;					// 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
-		is3DEN = 0;							// 1 to run init function in Eden Editor as well
-		curatorCanAttach = 0;				// 1 to allow Zeus to attach the module to an entity
+		isGlobal = 1;						// 0 = server, 1 = global, 2 = persistent global execution
+		isTriggerActivated = 0;				// 1 = wait for synced triggers
+		isDisposable = 1;					// 0 = repeatable
+		is3DEN = 0;							// 1 = run in 3DEN
+		curatorCanAttach = 0;				// 1 = drag and drop on entity
 
 		// 3DEN Attributes Menu Options
-		canSetArea = 0;						// Allows for setting the area values in the Attributes menu in 3DEN
-		canSetAreaShape = 0;				// Allows for setting "Rectangle" or "Ellipse" in Attributes menu in 3DEN
-		canSetAreaHeight = 0;				// Allows for setting height or Z value in Attributes menu in 3DEN
+		canSetArea = 0;						// 1 = allows Area Attributes
 
 		class Attributes : AttributesBase
-		{
-            class Units : Units
-			{
-				property = "IK_RescaleObjects_Units";
-			};
-			class ModuleDescription : ModuleDescription {};
-		};
-
+		{};
 		class ModuleDescription : ModuleDescription
 		{
-			description = "Sync objects you wish to resacle.";	// Short description, will be formatted as structured text
+			description = "Sync objects you wish to resacle.";	// Short description, structured text
 			sync[] = {"AnyObject"};				// Array of synced entities (can contain base classes)
 		};
 	};
