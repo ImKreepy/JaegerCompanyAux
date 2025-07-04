c184baddacb46e6abf55aed3a159f1ef15cdc8b3
Im Kreepy <53015099+ImKreepy@users.noreply.github.com>
2025-07-04 17:05:58 -0400

    Update config.cpp

--- addons/modules/config.cpp
+++ addons/modules/config.cpp
@@ -1,1 +1,16 @@
-lel
+#include "script_component.hpp"
+
+class CfgPatches
+{
+    class ADDON
+    {
+        name = COMPONENT_NAME;
+        author = AUTHOR;
+        authors[] = {"Im Kreepy"};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"jc_main"};
+        units[] = { };
+        weapons[] = {   };
+        VERSION_CONFIG;
+    };
+};
