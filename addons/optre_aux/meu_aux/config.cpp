70ef924e88ad03edc9612ae09743db5506e1a4fc
Im Kreepy <jkoscinski3@hotmail.com>
2025-07-04 02:34:44 -0400

    Added Riker's toy

--- addons/optre_aux/meu_aux/config.cpp
+++ addons/optre_aux/meu_aux/config.cpp
@@ -13,3 +13,29 @@ class CfgPatches
         VERSION_CONFIG;
     };
 };
+class CfgVehicles
+{
+	class I_UAV_01_F;
+	class MEU_Sentinel_A: I_UAV_01_F{};
+	class JC_MEU_Sentinel_Aggressor: MEU_Sentinel_A
+	{
+		side=2;
+		scope=2;
+		displayname="[JC] Aggressor Sentinel";
+		model="PR_Forerunner\SentinelAggressor\SentinelAggressor.p3d";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"glowall"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			QPATHTOF(SUBADDON\jc_sentinel_aggressor_co.paa)
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"PR_Forerunner\SentinelAggressor\data\sentinelAT.rvmat",
+			"PR_Forerunner\SentinelAggressor\data\emis.rvmat"
+		};
+	};
+};
