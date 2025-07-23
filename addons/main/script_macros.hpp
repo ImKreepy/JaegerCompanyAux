1757762b35ef91d9e8ae0c0df338b246e739df95
Im Kreepy <jkoscinski3@hotmail.com>
2025-07-23 04:08:07 -0400

    Refactor material path macros for improved clarity and organization

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -13,11 +13,23 @@
     #define PREPMAIN(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName)), QFUNCMAIN(fncName)] call CBA_fnc_compileFunction
 #endif
 
-#ifndef MATPATH_SYS
-    #define MATPATH_SYS(var1,var2,var3) MAINPREFIX\var1\SUBPREFIX\var2\var3.paa
-#endif
-#define MATPATH(var1) MATPATH_SYS(PREFIX,COMPONENT,var1)
-#define QMATPATH(var1) MATPATH(var1)
+/*---- MACRO chain to define a material path for rvmats. ----*/
+// Creating a pathing without a begining slash.
+#define PATHTO(var1) PATHTOF2_SYS(PREFIX,COMPONENT,var1)
+#define PATHTOSUB(var1) PATHTO(SUBCOMPONENT\var1)
+
+// Quoting the pathing.
+#define QPATHTO(var1) QUOTE(PATHTO(var1))
+#define QPATHTOSUB(var1) QUOTE(PATHTOSUB(var1))
+
+// Creating pathing to a rvmat folder.
+#define QPATHTOM(var1) QPATHTO(data\rvmats\var1.paa)
+#define QPATHTOSUBM(var1) QPATHTOSUB(data\rvmats\var1.paa)
+
+// Creating pathing to a rvmat folder in a subfolder.
+#define QPATHTOM2(var1,var2) QPATHTO(data\var1\rvmats\var2.paa)
+#define QPATHTOSUBM2(var1,var2) QPATHTOSUB(data\var1\rvmats\var2.paa)
+/*-------------------------------------------*/
 
 #define ITEM_NAME(NAME) QUOTE([##PREFIX_LONG##] NAME)
 #define ITEM_PICTURE(PICTURE) QUOTE(ik\jc\addons\main\item_picture\##PICTURE##.paa)
