a0847284468c89b73099a4cc07d489be073f9665
Im Kreepy <jkoscinski3@hotmail.com>
2025-07-30 02:21:13 -0400

    Removed my MACRO hard work be rvmats gay

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -13,30 +13,6 @@
     #define PREPMAIN(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName)), QFUNCMAIN(fncName)] call CBA_fnc_compileFunction
 #endif
 
-/*---- MACRO chain to define a material path in rvmats. ----*/
-// Creating a pathing without a begining slash.
-#ifdef PATHTO_R
-    #undef PATHTO_R
-    #define PATHTO_R(var1) PATHTOF2_SYS(PREFIX,COMPONENT,var1)
-#endif
-#define PATHTOSUB_R(var1) PATHTO_R(SUBCOMPONENT\var1)
-
-// Quoting the pathing.
-#ifdef QPATHTO_R
-    #undef QPATHTO_R
-    #define QPATHTO_R(var1) QUOTE(PATHTO_R(var1))
-#endif
-#define QPATHTOSUB_R(var1) QUOTE(PATHTOSUB(var1))
-
-// Creating pathing to a rvmat folder.
-#define QPATHTOM(var1) QPATHTO_R(data\rvmats\var1.paa)
-#define QPATHTOSUBM(var1) QPATHTOSUB_R(data\rvmats\var1.paa)
-
-// Creating pathing to a rvmat folder in a subfolder.
-#define QPATHTOM2(var1,var2) QPATHTO_R(data\var1\rvmats\var2.paa)
-#define QPATHTOSUBM2(var1,var2) QPATHTOSUB_R(data\var1\rvmats\var2.paa)
-/*-------------------------------------------*/
-
 #define QPATHTOSUBF(var1) QPATHTOF(SUBCOMPONENT\var1)
 
 #define ITEM_NAME(NAME) QUOTE([##PREFIX_LONG##] NAME)
