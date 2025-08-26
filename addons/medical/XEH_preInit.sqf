38b94a5143e2b53c57a578f9d06bd5e0e4c844a9
Im Kreepy <jkoscinski3@hotmail.com>
2025-08-26 14:40:26 -0400

    Medical files

---
+++ addons/medical/XEH_preInit.sqf
@@ -0,0 +1,9 @@
+#include "script_component.hpp"
+
+ADDON = false;
+
+INFO("Jaeger Company Medical Scripts Loading");
+#include "XEH_PREP.hpp"
+INFO("Jaeger Company Medical Scripts Loaded");
+
+ADDON = true;
---
+++ addons/medical/script_component.hpp
@@ -0,0 +1,7 @@
+#define COMPONENT medical
+#define COMPONENT_BEAUTIFIED Medical
+#include "\ik\jc\addons\main\script_mod.hpp"
+
+#include "\ik\jc\addons\main\script_macros.hpp"
+
+#define ACE_Medical QUTOE(ace_medical_engine),QUOTE(ace_medical),QUOTE(ace_medical_damage),QUOTE(ace_medical_treatment)
