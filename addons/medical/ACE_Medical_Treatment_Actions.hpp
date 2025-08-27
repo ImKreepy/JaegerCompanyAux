811c70532df206e22b480a6cb9561a7853e8014a
Im Kreepy <jkoscinski3@hotmail.com>
2025-08-27 15:17:55 -0400

    Added Treatment Action

--- addons/medical/ACE_Medical_Treatment_Actions.hpp
+++ addons/medical/ACE_Medical_Treatment_Actions.hpp
@@ -2,4 +2,22 @@ class ACE_Medical_Treatment_Actions {
     class BasicBandage {
         condition = QFUNC(canBandage);
     };
+    class RemoveImpalement: BasicBandage {
+        displayName = "Remove Impalement";
+        displayNameProgress = "Removing Impalement";
+        icon = QPATHTOF(ui\surgical_kit.paa);
+        category = "advanced";
+        items[] = {""};
+        treatmentLocations = QGVAR(locationPluck);
+        allowSelfTreatment = QGVAR(allowSelfPlucking);
+        medicRequired = QGVAR(medicPluck);
+        treatmentTime = QFUNC(getPluckTime);
+        condition = QFUNC(canPluck);
+        callbackSuccess = "";
+        callbackStart = QFUNC(pluckStart);
+        callbackProgress = QFUNC(pluckingProgress);
+        consumeItem = 0; // setting can be 0,1,2 - only 1 will consume items[]
+        animationMedic = "AinvPknlMstpSnonWnonDnon_medic1";
+        litter[] = {{""}};
+    };
 };
