734b3a4203bd20bd4e628aedd582b57be55c7b3f
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-02 01:57:19 -0400

    For testing sake

--- addons/medical/ACE_Medical_Injuries.hpp
+++ addons/medical/ACE_Medical_Injuries.hpp
@@ -12,7 +12,7 @@ class ACE_Medical_Injuries {
             thresholds[] = {{20, 10}, {4.5, 2}, {3, 1}, {0, 1}};
             selectionSpecific = 1;
             class Avulsion {
-                weighting[] = {{1.5, 1}, {0.35, 0}};
+                weighting[] = {{1, 1}, {0.35, 0}};
             };
             class Contusion {
                 weighting[] = {{0.35, 0}, {0.35, 1}};
@@ -20,7 +20,7 @@ class ACE_Medical_Injuries {
                 painMultiplier = 2.2;
             };
             class BlamiteWound {
-                weighting[] = {{1, 0}, {1, 1}, {0.35, 1}, {0.35, 0}};
+                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
                 sizeMultiplier = 0.8;
             };
         };
@@ -28,7 +28,7 @@ class ACE_Medical_Injuries {
             thresholds[] = {{4.5, 2}, {3, 1}, {0, 1}};
             selectionSpecific = 1;
             class PunctureWound {
-                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
+                weighting[] = {{1.5, 1}, {0.35, 1}};
                 sizeMultiplier = 0.8;
             };
         };
