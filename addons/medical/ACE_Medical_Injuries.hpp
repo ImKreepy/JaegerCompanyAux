a9cb8337eddd00013aa3e3499a3e5291455909d7
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-02 14:33:00 -0400

    Added additional wound and damage tpyes

--- addons/medical/ACE_Medical_Injuries.hpp
+++ addons/medical/ACE_Medical_Injuries.hpp
@@ -1,13 +1,31 @@
 class ACE_Medical_Injuries {
     class wounds {
-        class BlamiteWound {
+        class ImpalementWound {
             bleeding = 0.05;
             pain = 0.8;
             causeLimping = 1;
             causeFracture = 1;
         };
+        class BlamiteWound: ImpalementWound {};
+        class SpikeWound: ImpalementWound {};
     };
     class damageTypes {
+        class Impalement {
+            thresholds[] = {{20, 10}, {4.5, 2}, {3, 1}, {0, 1}};
+            selectionSpecific = 1;
+            class Avulsion {
+                weighting[] = {{1, 1}, {0.35, 0}};
+            };
+            class Contusion {
+                weighting[] = {{0.35, 0}, {0.35, 1}};
+                sizeMultiplier = 3.2;
+                painMultiplier = 2.2;
+            };
+            class ImpalementWound {
+                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
+                sizeMultiplier = 0.8;
+            };
+        };
         class Blamite {
             thresholds[] = {{20, 10}, {4.5, 2}, {3, 1}, {0, 1}};
             selectionSpecific = 1;
@@ -20,6 +38,22 @@ class ACE_Medical_Injuries {
                 painMultiplier = 2.2;
             };
             class BlamiteWound {
+                weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
+                sizeMultiplier = 0.8;
+            };
+        };
+        class Spike {
+            thresholds[] = {{20, 10}, {4.5, 2}, {3, 1}, {0, 1}};
+            selectionSpecific = 1;
+            class Avulsion {
+                weighting[] = {{1, 1}, {0.35, 0}};
+            };
+            class Contusion {
+                weighting[] = {{0.35, 0}, {0.35, 1}};
+                sizeMultiplier = 3.2;
+                painMultiplier = 2.2;
+            };
+            class SpikeWound {
                 weighting[] = {{1.5, 0}, {1.5, 1}, {0.35, 1}, {0.35, 0}};
                 sizeMultiplier = 0.8;
             };
--- addons/medical/ACE_Medical_Treatment.hpp
+++ addons/medical/ACE_Medical_Treatment.hpp
@@ -1,59 +1,109 @@
 class ACE_Medical_Treatment {
     class Bandaging {
         class BasicBandage {
-            class Impalement {
+            class ImpalementWound {
                 effectiveness = 0;
                 reopeningChance = 0;
                 reopeningMinDelay = 0;
                 reopeningMaxDelay = 0;
             };
-            class ImpalementMinor : Impalement {};
-            class ImpalementMedium : Impalement {};
-            class ImpalementLarge : Impalement {};
+            class ImpalementWoundMinor : ImpalementWound {};
+            class ImpalementWoundMedium : ImpalementWound {};
+            class ImpalementWoundLarge : ImpalementWound {};
+
+            class BlamiteWound : ImpalementWound {};
+            class BlamiteWoundMinor : BlamiteWound {};
+            class BlamiteWoundMedium : BlamiteWound {};
+            class BlamiteWoundLarge : BlamiteWound {};
+            
+            class SpikeWound : ImpalementWound {};
+            class SpikeWoundMinor : SpikeWound {};
+            class SpikeWoundMedium : SpikeWound {};
+            class SpikeWoundLarge : SpikeWound {};
         };
         class FieldDressing {
-            class Impalement {
+            class ImpalementWound {
                 effectiveness = 0;
                 reopeningChance = 0;
                 reopeningMinDelay = 0;
                 reopeningMaxDelay = 0;
             };
-            class ImpalementMinor : Impalement {};
-            class ImpalementMedium : Impalement {};
-            class ImpalementLarge : Impalement {};
+            class ImpalementWoundMinor : ImpalementWound {};
+            class ImpalementWoundMedium : ImpalementWound {};
+            class ImpalementWoundLarge : ImpalementWound {};
+
+            class BlamiteWound : ImpalementWound {};
+            class BlamiteWoundMinor : BlamiteWound {};
+            class BlamiteWoundMedium : BlamiteWound {};
+            class BlamiteWoundLarge : BlamiteWound {};
+            
+            class SpikeWound : ImpalementWound {};
+            class SpikeWoundMinor : SpikeWound {};
+            class SpikeWoundMedium : SpikeWound {};
+            class SpikeWoundLarge : SpikeWound {};
         };
         class PackingBandage : FieldDressing {
-            class Impalement {
+            class ImpalementWound {
                 effectiveness = 0;
                 reopeningChance = 0;
                 reopeningMinDelay = 0;
                 reopeningMaxDelay = 0;
             };
-            class ImpalementMinor : Impalement {};
-            class ImpalementMedium : Impalement {};
-            class ImpalementLarge : Impalement {};
+            class ImpalementWoundMinor : ImpalementWound {};
+            class ImpalementWoundMedium : ImpalementWound {};
+            class ImpalementWoundLarge : ImpalementWound {};
+
+            class BlamiteWound : ImpalementWound {};
+            class BlamiteWoundMinor : BlamiteWound {};
+            class BlamiteWoundMedium : BlamiteWound {};
+            class BlamiteWoundLarge : BlamiteWound {};
+            
+            class SpikeWound : ImpalementWound {};
+            class SpikeWoundMinor : SpikeWound {};
+            class SpikeWoundMedium : SpikeWound {};
+            class SpikeWoundLarge : SpikeWound {};
         };
         class ElasticBandage : FieldDressing {
-            class Impalement {
+            class ImpalementWound {
                 effectiveness = 0;
                 reopeningChance = 0;
                 reopeningMinDelay = 0;
                 reopeningMaxDelay = 0;
             };
-            class ImpalementMinor : Impalement {};
-            class ImpalementMedium : Impalement {};
-            class ImpalementLarge : Impalement {};
+            class ImpalementWoundMinor : ImpalementWound {};
+            class ImpalementWoundMedium : ImpalementWound {};
+            class ImpalementWoundLarge : ImpalementWound {};
+
+            class BlamiteWound : ImpalementWound {};
+            class BlamiteWoundMinor : BlamiteWound {};
+            class BlamiteWoundMedium : BlamiteWound {};
+            class BlamiteWoundLarge : BlamiteWound {};
+            
+            class SpikeWound : ImpalementWound {};
+            class SpikeWoundMinor : SpikeWound {};
+            class SpikeWoundMedium : SpikeWound {};
+            class SpikeWoundLarge : SpikeWound {};
         };
         class QuikClot : FieldDressing {
-            class Impalement {
+            class ImpalementWound {
                 effectiveness = 0;
                 reopeningChance = 0;
                 reopeningMinDelay = 0;
                 reopeningMaxDelay = 0;
             };
-            class ImpalementMinor : Impalement {};
-            class ImpalementMedium : Impalement {};
-            class ImpalementLarge : Impalement {};
+            class ImpalementWoundMinor : ImpalementWound {};
+            class ImpalementWoundMedium : ImpalementWound {};
+            class ImpalementWoundLarge : ImpalementWound {};
+
+            class BlamiteWound : ImpalementWound {};
+            class BlamiteWoundMinor : BlamiteWound {};
+            class BlamiteWoundMedium : BlamiteWound {};
+            class BlamiteWoundLarge : BlamiteWound {};
+            
+            class SpikeWound : ImpalementWound {};
+            class SpikeWoundMinor : SpikeWound {};
+            class SpikeWoundMedium : SpikeWound {};
+            class SpikeWoundLarge : SpikeWound {};
         };
     };
 };
