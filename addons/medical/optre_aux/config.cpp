156e7fa95d8511ba7701386cd12e767024795421
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-03 01:08:57 -0400

    Added reqs

--- addons/medical/optre_aux/config.cpp
+++ addons/medical/optre_aux/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         name = COMPONENT_NAME;
         addonRootClass = QUOTE(ADDON);
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QUOTE(ADDON),"OPTRE_FC_Weapons","OPTRE_FC_Weapons_Ammo","OPTRE_FC_Weapons_Needle_Mine"};
+        requiredAddons[] = {QUOTE(ADDON),"OPTRE_FC_Weapons","OPTRE_FC_Weapons_Ammo",NEEDLE_REQS};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
--- addons/medical/optre_aux/script_component.hpp
+++ addons/medical/optre_aux/script_component.hpp
@@ -1,3 +1,5 @@
 #define SUBCOMPONENT optre_aux
 #define SUBCOMPONENT_BEAUTIFIED Operation: TREBUCHETE Auxiliary
 #include "..\script_component.hpp"
+
+#define NEEDLE_REQS QUOTE(OPTRE_FC_Weapons_Needler),QUOTE(OPTRE_FC_Weapons_Needle_Mine),QUOTE(OPTRE_FC_Weapons_Needle_Mine_Dispenser)
