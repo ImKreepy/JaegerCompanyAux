102aff4b1b08caa4ccdb8998aba003d2645a2a6f
Im Kreepy <jkoscinski3@hotmail.com>
2025-09-03 00:59:13 -0400

    Added required addons

--- addons/medical/optre_aux/config.cpp
+++ addons/medical/optre_aux/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         name = COMPONENT_NAME;
         addonRootClass = QUOTE(ADDON);
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QUOTE(ADDON)};
+        requiredAddons[] = {QUOTE(ADDON),"OPTRE_FC_Weapons","OPTRE_FC_Weapons_Ammo","OPTRE_FC_Weapons_Needle_Mine"};
         units[] = {};
         weapons[] = {};
         VERSION_CONFIG;
