75713996877fd735c33fd2eca65bc48ef13b7a23
Im Kreepy <jkoscinski3@hotmail.com>
2025-12-11 14:57:19 -0500

    #

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,3 +1,3 @@
 #define MAJOR 2// Major Update, used for massive changes
-#define MINOR 3// Minor Update, used for new features and improvements
+#define MINOR 4// Minor Update, used for new features and improvements
 #define PATCH 0// Patch level, used for bug fixes and minor improvements
